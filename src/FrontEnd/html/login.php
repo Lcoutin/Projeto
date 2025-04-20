6d89484382211e3b2d252ac94e445e1695b3d292
Lcoutin <lucas.coutinho46@hotmail.com>
2025-04-20 17:44:20 -0400

    atualizando captcha

--- src/FrontEnd/html/login.php
+++ src/FrontEnd/html/login.php
@@ -44,15 +44,16 @@
                     </div>
                 
                     <button type="submit" id="button-login">Entrar</button>
-                    <!-- Adiciona o script do reCAPTCHA v2 -->
-<script src="https://www.google.com/recaptcha/api.js" async defer></script>
+                    <script src="https://www.google.com/recaptcha/api.js" async defer></script>
 
 <form action="/verificar" method="POST">
   <!-- Componente do reCAPTCHA -->
   <div class="g-recaptcha" data-sitekey="SUA_SITE_KEY_AQUI"></div>
   <br/>
   <input type="submit" value="Enviar">
-</form>
+</form>                 
+
+
 
                 </form>
                 
