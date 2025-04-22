74a5d731f072c4f4eefffa7f104dcf0b0edb023f
Lcoutin <148804367+Lcoutin@users.noreply.github.com>
2025-04-22 10:17:54 -0400

    summir

--- src/FrontEnd/html/login.php
+++ src/FrontEnd/html/login.php
@@ -1,18 +1,7 @@
 <?php include '../../FrontEnd/html/acessibilidade.html';?>
 <!DOCTYPE html>
 <html lang="pt-BR">
-<head>
-    <title>reCAPTCHA demo: Simple page</title>
-    <script src="https://www.google.com/recaptcha/api.js" async defer></script>
-  </head>
-  <body>
-    <form action="?" method="POST">
-      <div class="g-recaptcha" data-sitekey="your_site_key"></div>
-      <br/>
-      <input type="submit" value="Submit">
-    </form>
-  </body>
-</html>    
+    
 
 <head>
     <meta charset="UTF-8">
