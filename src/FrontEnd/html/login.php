2f1956a0a0ff7731462e2791dcbd56ec3894cfcb
Lcoutin <148804367+Lcoutin@users.noreply.github.com>
2025-04-22 10:10:17 -0400

    commit

--- src/FrontEnd/html/login.php
+++ src/FrontEnd/html/login.php
@@ -44,17 +44,7 @@
                     </div>
                     <html>
                     <head>
-    <title>reCAPTCHA demo: Simple page</title>
-    <script src="https://www.google.com/recaptcha/api.js" async defer></script>
-  </head>
-  <body>
-    <form action="?" method="POST">
-      <div class="g-recaptcha" data-sitekey="your_site_key"></div>
-      <br/>
-      <input type="submit" value="Submit">
-    </form>
-  </body>
-</html>
+  
                 
                     <button type="submit" id="button-login">Entrar</button>
                     <script src="https://www.google.com/recaptcha/api.js" async defer></script>
